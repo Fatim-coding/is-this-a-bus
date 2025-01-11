32d44dc60cdff045f87eedb7f1c6bb114f03522d
Fatim-coding <timabadjie12@gmail.com>
2025-01-11 15:36:02 +0000

    second commit

---
+++ main2.py
@@ -0,0 +1,24 @@
+# python code to demostrate hwo parent construction
+# sre called
+
+# parent class
+class Person ( object ):
+
+    def __init__(self, name, idnumber):
+        self.name = name 
+        self. idnumber = idnumber
+    def display(self):
+        print(self.name)
+        print(self.idnumber)
+
+class Employee( Person ):
+    def __init__(self, name, idnumber, salary, post):
+        self.salary = salary
+        self.post = post
+
+        Person.__init__(self, name, idnumber)
+
+
+a = Employee('Rahul', 886012, 200000, "Intern")
+
+a.display()
